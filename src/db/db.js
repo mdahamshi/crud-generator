7f72465fdb7cadfd8d8ff52e046e233907b0a4e2
Mohammad Dahamshi <mohammad.dahamshi@gmail.com>
2025-08-07 14:53:29 +0300

    remove bloat

--- src/db/db.js
+++
@@ -1,7 +0,0 @@
-import author from './queries/author.js';
-import book from './queries/book.js';
-
-const db = {
-  author,  book,};
-
-export default db;
--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import authorsRoutes from './authors.js';
-import booksRoutes from './books.js';
-
-function registerRoutes(app, apiV) {
-  app.use(`/api/${apiV}/authors`, authorsRoutes);  app.use(`/api/${apiV}/books`, booksRoutes);
-}
-
-export default registerRoutes;
